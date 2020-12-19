f26bd5324fff74ec0f3a3f4b0b920fd598e2af78
John Foster <johnfost.361@gmail.com>
2020-12-19 16:47:14 -0500

    Add project files.

---
+++ gameOver2/gameOver2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; // this says that you are using the std namespace, so any elements from std will be known to be from std
+
+int main()
+{
+    cout << "Game Over 2!" << endl;
+    return 0;
+}
