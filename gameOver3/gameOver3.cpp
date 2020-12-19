c5a8bfce07bd884a8630a6e88113e746d72207c0
John Foster <johnfost.361@gmail.com>
2020-12-19 16:58:35 -0500

    Added gameOver3

---
+++ gameOver3/gameOver3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+using std::endl; // declarations. this specifies which parts of the std namespace you will use
+
+int main()
+{
+    cout << "Game Over 3!" << endl;
+}
